6da2b96b75e1c4ab96b3e4a10a0298995962926c
root <root@debian>
2014-03-22 23:47:28 -0400

    new start

--- issues/settings.py
+++ issues/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # WHO'S USING???
-JAN_USING = False
+JAN_USING = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
